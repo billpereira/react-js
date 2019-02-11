59ac2916a04bc49438d073fe50f5f999fad9ce87
William Pereira <william.pereira@me.com>
2019-02-11 14:45:24 -0200

    starting boilerplate-v1

---
+++ expensify-app/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+const Header = () => (
+	<header>
+		<h1>Expensify</h1>
+		<p>
+			<NavLink to="/" activeClassName="is-active" exact={true}>
+				{' '}
+				dashboard{' '}
+			</NavLink>
+			<NavLink to="/create" activeClassName="is-active">
+				{' '}
+				Create{' '}
+			</NavLink>
+			<NavLink to="/help" activeClassName="is-active">
+				{' '}
+				Help{' '}
+			</NavLink>
+		</p>
+	</header>
+);
+
+export default Header;
